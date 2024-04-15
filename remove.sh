3ee8645be053a239e62f5566e46f10cc86936b9d
Peter Kester <peter.kester@koop.overheid.nl>
2024-04-15 09:27:44 +0200

    adjusted some parameters

--- remove.sh
+++ remove.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 source .env
-docker compose -f marklogic-3n-centos.yaml -p $DOCKERPROJECT down
+docker compose -f marklogic-3n-centos.yaml -p $DOCKERPROJECT down --volumes
+
+rm -rf data/MarkLogic1/export
+rm -rf data/MarkLogic1/Logs
+rm -rf data/MarkLogic2/export
+rm -rf data/MarkLogic2/Logs
+rm -rf data/MarkLogic3/Logs
